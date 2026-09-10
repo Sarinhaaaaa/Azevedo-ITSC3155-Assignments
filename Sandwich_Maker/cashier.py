7ea2329e1a59ea420a96abd316969d7e4e87ed20
Sarah Azevedo <sazevedo@charlotte.edu>
2026-09-10 16:29:13 -0400

    fix: fixed transaction_result miscalculation line 19.

--- Sandwich_Maker/cashier.py
+++ Sandwich_Maker/cashier.py
@@ -16,12 +16,12 @@ class Cashier:
 
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient."""
-        if coins > cost:
+        if coins >= cost:
             change = round(coins - cost, 2)
             print(f"Here is ${change} in change.")
             return True
         else:
-            print("Sorry, that’s not enough money. Money refunded.")
+            print("Sorry that’s not enough money. Money refunded.")
             return False
 
 
