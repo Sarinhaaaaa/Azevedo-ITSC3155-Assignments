84206b9233078fb3e3e1b6e7a4532efc13777ecb
Sarah Azevedo <sazevedo@charlotte.edu>
2026-09-10 13:40:23 -0400

    feat: add initial machine resources and sandwich recipes

---
+++ Sandwich_Maker/main.py
@@ -0,0 +1,37 @@
+import data
+from sandwich_maker import SandwichMaker
+from cashier import Cashier
+
+
+# Make an instance of other classes here
+resources = data.resources
+recipes = data.recipes
+sandwich_maker_instance = SandwichMaker(resources)
+cashier_instance = Cashier()
+
+def main():
+    machine_on = True
+
+    while machine_on:
+        choice = input("What would you like? (small/medium/large/off/report): ")
+
+        if choice == "off":
+            machine_on = False
+        elif choice == "report":
+            print(f"Bread: {sandwich_maker_instance.machine_resources['bread']} slice(s)")
+            print(f"Ham: {sandwich_maker_instance.machine_resources['ham']} slice(s)")
+            print(f"Cheese: {sandwich_maker_instance.machine_resources['cheese']} pound(s)")
+        elif choice in ["small", "medium", "large"]:
+            sandwich = recipes[choice]
+
+            if sandwich_maker_instance.check_resources(sandwich["ingredients"]):
+                payment = cashier_instance.process_coins()
+
+                if cashier_instance.transaction_result(payment, sandwich["cost"]):
+                    sandwich_maker_instance.make_sandwich(choice, sandwich["ingredients"])
+
+        else:
+            print("Invalid input. Please try again.")
+
+if __name__=="__main__":
+    main()
